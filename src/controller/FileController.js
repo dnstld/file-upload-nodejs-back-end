a321f55071bf705a4aa5101bef113e40562e9908
Denis Toledo <contato@denistoledo.com.br>
2019-04-10 00:07:45 -0300

    routing

--- src/controller/FileController.js
+++ src/controller/FileController.js
@@ -1,9 +1,19 @@
+const Box = require('../models/Box')
 const File = require('../models/File')
 
 class FileController {
   async store(req, res) {
-    console.log(req.file)
-    return res.send('OK')
+    const box = await Box.findById(req.params.id)
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key,
+    })
+
+    box.files.push(file)
+
+    await box.save()
+
+    return res.json(file)
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,12 @@ const BoxController = require('./controller/BoxController')
 const FileController = require('./controller/FileController')
 
 routes.post('/boxes', BoxController.store)
-routes.post('/files', multer(multerConfig).single('file'), FileController.store)
+routes.get('/boxes/:id', BoxController.show)
+
+routes.post(
+  '/boxes/:id/files', 
+  multer(multerConfig).single('file'), 
+  FileController.store
+)
 
 module.exports = routes
