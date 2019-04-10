94f839913158cec85cbff30326386588e9ee128e
Denis Toledo <contato@denistoledo.com.br>
2019-04-09 23:10:47 -0300

    add mongoose db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb+srv://upload:upload1981DnsTld@cluster0-vq0qy.mongodb.net/upload?retryWrites=true', {
+  useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
